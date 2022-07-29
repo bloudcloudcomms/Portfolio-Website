94be6f22f87d76ad010fb362a7893140445a4629
bloudcloudcomms <55114700+bloudcloudcomms@users.noreply.github.com>
2022-07-29 10:37:57 -0400

    added files

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// activating mobile button menu
+var menuBtn = document.getElementsByClassName("menu-btn");
+var mobileMenu = document.getElementsByClassName("mobile-menu");
+var clickedBtn = function () {
+  mobileMenu[0].classList.toggle("active");
+};
+
+menuBtn[0].addEventListener("click", clickedBtn);
